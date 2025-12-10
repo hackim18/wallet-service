283852ec6a606310e14ed7edfd8da5017eee4dd3
Hackim18 <khakim668@gmail.com>
2025-12-10 13:31:41 +0700

    fix: fixing common route response message

--- internal/delivery/http/route/common_route.go
+++ internal/delivery/http/route/common_route.go
@@ -2,20 +2,21 @@ package route
 
 import (
 	"net/http"
+	"wallet-service/internal/constants"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (c *RouteConfig) RegisterCommonRoutes(app *gin.Engine) {
 	welcomeHandler := func(ctx *gin.Context) {
-		res := gin.H{"message": "Welcome to Wallet Service API"}
+		res := gin.H{"message": constants.WelcomeMessage}
 		ctx.JSON(http.StatusOK, res)
 	}
 
 	app.GET("/", welcomeHandler)
 	app.GET("/api", welcomeHandler)
 	app.NoRoute(func(ctx *gin.Context) {
-		res := gin.H{"message": "API not found"}
+		res := gin.H{"message": constants.NotFound}
 		ctx.AbortWithStatusJSON(http.StatusNotFound, res)
 	})
 }
